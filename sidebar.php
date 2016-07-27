d8165c28d91f70dc2be486cc1cf9c60b98fe1894
samuel jean <samueljean09@gmail.com>
2016-07-27 15:59:19 -0400

    Initial commit

---
+++ sidebar.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @package _tk
+ */
+?>
+
+	</div><!-- close .main-content-inner -->
+
+	<div class="sidebar ">
+
+		<?php // add the class "panel" below here to wrap the sidebar in Bootstrap style ;) ?>
+
